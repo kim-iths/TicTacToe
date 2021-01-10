24bf84b69d60990376431f2f1ea0877c7ab7b851
kim-iths <kim.hellman@iths.se>
2021-01-11 00:27:01 +0100

    added imageviews, added gesturerecognizers to each one, added images for O's and X's

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stackViewH: UIStackView!
+    
+    @IBOutlet var images: [UIImageView]!
+    
+    var isBlue = true
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        for i in 0...images.count-1 {
+            images[i].isUserInteractionEnabled = true
+            images[i].addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(imageTapped(tgr:))))
+        }
+        
     }
-
-
+    
+    @objc func imageTapped(tgr: UITapGestureRecognizer) {
+        let tappedImage = tgr.view as! UIImageView
+        
+        if isBlue {
+            tappedImage.image = UIImage(named: "o")
+            isBlue = false
+        } else {
+            tappedImage.image = UIImage(named: "x")
+            isBlue = true
+        }
+        
+    }
+    
+    
 }
-
